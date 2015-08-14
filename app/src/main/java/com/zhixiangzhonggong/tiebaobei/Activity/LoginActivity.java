f6bae89c88fc91dd024f860afae1abaaf30e50da
yao_mac <yaolu0796@gmail.com>
2015-08-14 02:37:31 -0400

    login page

---
+++ app/src/main/java/com/zhixiangzhonggong/tiebaobei/Activity/LoginActivity.java
@@ -0,0 +1,73 @@
+package com.zhixiangzhonggong.tiebaobei.Activity;
+
+import android.app.Activity;
+import android.graphics.Paint;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.zhixiangzhonggong.tiebaobei.R;
+
+public class LoginActivity extends Activity {
+    private TextView forgetPassWorldText;
+    private ImageButton checkButton;
+    private boolean colorChanged=false;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        initView();
+
+        checkButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!colorChanged ) {
+                    checkButton.setImageResource(R.drawable.check);
+                    colorChanged = true;
+                } else  {
+                    checkButton.setImageResource(R.drawable.uncheck);
+                    colorChanged = false;
+
+                }
+
+
+            }
+        });
+    }
+
+    private void initView() {
+        forgetPassWorldText= (TextView) findViewById(R.id.forgotPasswordText);
+        forgetPassWorldText .getPaint().setFlags(Paint. UNDERLINE_TEXT_FLAG );
+        checkButton=(ImageButton)findViewById(R.id.checkImageButton);
+        checkButton.setImageResource(R.drawable.uncheck);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_login, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/com/zhixiangzhonggong/tiebaobei/Activity/MainActivity.java
+++ app/src/main/java/com/zhixiangzhonggong/tiebaobei/Activity/MainActivity.java
@@ -1,6 +1,7 @@
 package com.zhixiangzhonggong.tiebaobei.Activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,6 +10,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.zhixiangzhonggong.tiebaobei.CustomizedView.SlidingMenu;
 import com.zhixiangzhonggong.tiebaobei.R;
@@ -16,6 +19,8 @@ import com.zhixiangzhonggong.tiebaobei.R;
 public class MainActivity extends Activity {
     private ImageButton settingButton;
     private SlidingMenu mSlidingMenu;
+    private TextView mLogin;
+    private ImageView mBySmallPeopleImageLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
        requestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -24,6 +29,24 @@ public class MainActivity extends Activity {
         //delete actionbar
         setContentView(R.layout.activity_main);
         initView();
+        mBySmallPeopleImageLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+        });
+        mLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+        });
         settingButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -36,6 +59,9 @@ public class MainActivity extends Activity {
     private void initView() {
         mSlidingMenu= (SlidingMenu) findViewById(R.id.slidingMenu);
         settingButton= (ImageButton) findViewById(R.id.setting_btn);
+        mLogin= (TextView) findViewById(R.id.login_left_menu_id);
+        mBySmallPeopleImageLogin= (ImageView) findViewById(R.id.login_small_people_image);
+
     }
 
     @Override
